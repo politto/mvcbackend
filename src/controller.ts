0e60281a865ddc2a04abc97d1ae248aaf04cd397
nattaphat <eakacit@gmail.com>
2024-09-15 09:14:36 +0700

    first commit

---
+++ src/controller.ts
@@ -0,0 +1,12 @@
+import { Elysia } from "elysia";
+
+const app = new Elysia()
+.get("/", () => {
+  
+})
+.listen(3000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
+
